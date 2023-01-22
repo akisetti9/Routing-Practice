989137f1542dff291c2a6fa0b8a9ee2123edd74e
akisetti9 <saitejax8e@gmail.com>
2023-01-22 11:33:41 +0000

    third

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,14 +1,25 @@
 // Write your JS code here
+import {Component} from 'react'
+
 import './index.css'
 
-const NotFound = () => (
-  <div className="not-found-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
-      alt="not-found"
-      className="not-found-img"
-    />
-  </div>
-)
+class NotFound extends Component {
+  componentDidMount() {
+    const {history} = this.props
+    history.replace('/bad-path')
+  }
+
+  render() {
+    return (
+      <div className="not-found-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+          alt="not-found"
+          className="not-found-img"
+        />
+      </div>
+    )
+  }
+}
 
 export default NotFound
